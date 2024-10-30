bc1908bdc4a3bb6c3f98cb73f6ca0bf5a002637d
Yazeed <20418077ksa@aou.edu.sa>
2024-10-30 18:45:21 +0300

    fixing mySql script

--- mysql-setup.sh
+++ mysql-setup.sh
@@ -6,4 +6,4 @@ sudo apt-get install -yy git curl
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 
-docker run -d -p 3306:3306 --name mysqlimage mysql:latest
+docker run -d -p 3306:3306 --name mysqlimage -e MYSQL_ROOT_PASSWORD=rootpassword mysql:latest
